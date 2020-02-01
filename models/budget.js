e0f27987540007c9cf136a96b9aee4e4d22d6922
Jiahui Wang <wjh4289042@hotmail.com>
2020-02-01 07:33:08 -0800

    add code from week 17

--- models/budget.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const Schema = mongoose.Schema;
-
-const budgetSchema = new Schema();
-
-const Budget = mongoose.model("BUdget", BudgetSchema);
-module.export = Budget;
--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
-
 const app = express();
+const PORT = process.env.PORT || 4000;
 
 app.use(logger("dev"));
 
@@ -13,14 +12,19 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static("client"));
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
+// Requiring our models for syncing
+// const db = require("./models");
+
 // routes here
+app.use(require("./routes/api-routes.js"));
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
