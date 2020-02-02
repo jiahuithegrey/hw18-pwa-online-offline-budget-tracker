6fdcfeebe7c641bb1cb8d54a1ad426fab1d72ea6
Jiahui Wang <wjh4289042@hotmail.com>
2020-02-01 22:40:18 -0800

    heroku2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.MOGODB_URI || 4000;
 
 app.use(logger("dev"));
 
